2c612b7a1f11e807ad6d0912bf8b8af575c9896f
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-01-04 22:00:39 +1100

    fix: bad assignment

--- zsh-gcloud.plugin.zsh
+++ zsh-gcloud.plugin.zsh
@@ -9,9 +9,9 @@ fi
 
 # Identify location of completion include file, which is different depending on the OS type
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    local COMPLETION_INCLUDE = "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+    local COMPLETION_INCLUDE="/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 else
-    local COMPLETION_INCLUDE = "/usr/share/google-cloud-sdk/completion.zsh.inc"
+    local COMPLETION_INCLUDE="/usr/share/google-cloud-sdk/completion.zsh.inc"
 fi
 
 # If the completion include file exists, source it
