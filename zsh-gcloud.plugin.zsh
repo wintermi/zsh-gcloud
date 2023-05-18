9d593d75088cc275a3358f1f001482977be542d5
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-05-18 14:36:43 +1000

    feat: update to support the changes made within the Google Cloud SDK brew installer

--- zsh-gcloud.plugin.zsh
+++ zsh-gcloud.plugin.zsh
@@ -3,18 +3,30 @@
 
 # Exit if the 'gcloud' command can not be found
 if ! (( $+commands[gcloud] )); then
-    echo "ERROR: 'gcloud' command not found"
+    echo "WARNING: 'gcloud' command not found"
     return
 fi
 
-# Identify location of completion include file, which is different depending on the OS type
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    local COMPLETION_INCLUDE="/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-else
-    local COMPLETION_INCLUDE="/usr/share/google-cloud-sdk/completion.zsh.inc"
-fi
+# Identify the location of the Google Cloud SDK installed
+local DEFAULT_SDK_PATHS=("/opt/homebrew/share/google-cloud-sdk" \
+                         "/usr/share/google-cloud-sdk")
+for sp in "${DEFAULT_SDK_PATHS[@]}"; do
+    if [ -d "$sp" ]; then
+        local SDK_PATH="$sp"
+        break
+    fi
+done
 
-# If the completion include file exists, source it
-if [[ -f "$COMPLETION_INCLUDE" ]]; then
-    source "$COMPLETION_INCLUDE"
+# If the Google Cloud SDK path was found
+if [[ ! -z "$SDK_PATH" ]]; then
+    # If the path include file exists, source it
+    if [[ -f "$SDK_PATH/path.zsh.inc" ]]; then
+        typeset -TUx PATH path
+        source "$SDK_PATH/path.zsh.inc"
+    fi
+
+    # If the completion include file exists, source it
+    if [[ -f "$SDK_PATH/completion.zsh.inc" ]]; then
+        source "$SDK_PATH/completion.zsh.inc"
+    fi
 fi
